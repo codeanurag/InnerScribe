8711c7618c01d6e4e61c77084a5991c6115bb77a
Anurag <codeanuragpandit@gmail.com>
2025-07-09 23:20:41 +0530

    [InnerScribe] Quick update - 2 file(s) modified - 2025-07-09 23:20:41

---
+++ InnerScribe/ViewModels/JournalViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  JournalViewModel.swift
+//  InnerScribe
+//
+//  Created by Anurag Pandit on 09/07/25.
+//
+
+
+import Foundation
+
+class JournalViewModel: ObservableObject {
+    @Published var entries: [JournalEntry] = []
+
+    func addEntry(text: String) {
+        let score = SentimentAnalyzer.analyze(text: text)
+        let entry = JournalEntry(id: UUID(), date: Date(), text: text, sentimentScore: score)
+        entries.insert(entry, at: 0)
+        saveEntries()
+    }
+
+    private func getFileURL() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        return path.appendingPathComponent("entries.json")
+    }
+
+    private func saveEntries() {
+        let url = getFileURL()
+        if let data = try? JSONEncoder().encode(entries) {
+            try? data.write(to: url)
+        }
+    }
+
+    func loadEntries() {
+        let url = getFileURL()
+        if let data = try? Data(contentsOf: url),
+           let decoded = try? JSONDecoder().decode([JournalEntry].self, from: data) {
+            self.entries = decoded
+        }
+    }
+}
