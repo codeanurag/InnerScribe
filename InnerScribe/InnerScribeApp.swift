388ee2341de19167f2a4a074463632635e9e9dc4
Anurag <codeanuragpandit@gmail.com>
2025-07-09 23:17:52 +0530

    first commit

---
+++ InnerScribe/InnerScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnerScribeApp.swift
+//  InnerScribe
+//
+//  Created by Anurag Pandit on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct InnerScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InnerScribeTests/InnerScribeTests.swift
@@ -0,0 +1,17 @@
+//
+//  InnerScribeTests.swift
+//  InnerScribeTests
+//
+//  Created by Anurag Pandit on 09/07/25.
+//
+
+import Testing
+@testable import InnerScribe
+
+struct InnerScribeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
